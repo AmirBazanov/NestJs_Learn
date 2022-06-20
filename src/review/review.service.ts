1b21504ba44afddef3ce1e4a913faee18fd82dae
AmirBazanov <flash5651311@gmail.com>
2022-06-20 18:12:25 +0500

    Add service for mongo

---
+++ src/review/review.service.ts
@@ -0,0 +1,10 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ModelType } from '@typegoose/typegoose/lib/types';
+import { ReviewModel } from './review.model';
+
+@Injectable()
+export class ReviewService {
+    constructor(@Inject(ReviewModel) private readonly reviewModel: ModelType<ReviewModel>) {
+
+    }
+}
